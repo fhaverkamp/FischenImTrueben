07035a5d13fb385e7b8939325cd32384156959f6
Frank Haverkamp <haver@linux.vnet.ibm.com>
2016-03-15 11:15:15 +0100

    Try to connect to IoT server to get data from my RaspberryPI

--- server.py
+++ server.py
@@ -1,21 +1,39 @@
 import os
+from read_sensor import Control
+
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
+    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+    from SocketServer import TCPServer as Server
 except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
+
+class MyHandler(Handler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        """Respond to a GET request."""
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        Handler.do_GET(self)
 
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
+
 # Change current directory to avoid exposure of control files
 os.chdir('static')
 
+control = Control()
 httpd = Server(("", PORT), Handler)
 try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
+    print("Start serving at port %i" % PORT)
+    httpd.serve_forever()
 except KeyboardInterrupt:
-  pass
+    pass
+
 httpd.server_close()
 
